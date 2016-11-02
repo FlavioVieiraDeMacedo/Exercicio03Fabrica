fe2f1e9f2738a827c8ae0507f8ccc0d551a7efb5
Flavio Macedo <f.m.kmacedo@hotmail.com>
2016-11-02 01:59:13 -0200

    Primeiro Commit

---
+++ Exercicio03/Controllers/ProdutoController.cs
@@ -0,0 +1,35 @@
+﻿using Exercicio03.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Exercicio03.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private FabricaBDEntities _context = new FabricaBDEntities();
+        // GET: Produto
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(Produto p)
+        {
+            _context.Produto.Add(p);
+            _context.SaveChanges();
+            TempData["msg"] = "Produto Cadastrado";
+            return RedirectToAction("Cadastrar");
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            List<Produto> _lista = _context.Produto.ToList();
+            return View(_lista);
+        }
+    }
+}
---
+++ FabricaBD/Produto.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Produto]
+(
+	[ProdutoId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [DataCadastro] DATETIME NOT NULL, 
+    [Valor] DECIMAL NOT NULL, 
+    [Descricao] NVARCHAR(255) NULL, 
+    [Titulo] NVARCHAR(150) NOT NULL, 
+    [Nacional] BIT NOT NULL
+)
+
+
